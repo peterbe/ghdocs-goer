75f1fb2b3c0920029ca550e2b5ec32a7dea7d26f
Peter Bengtsson <mail@peterbe.com>
2022-09-08 08:03:04 -0400

    prettier and slight refactor

--- src/extension.ts
+++ src/extension.ts
@@ -10,64 +10,63 @@ export function activate(context: vscode.ExtensionContext) {
   // This line of code will only be executed once when your extension is activated
   // console.log('Congratulations, your extension "ghdocs-goer" is now active!');
 
-  function getCurrentURI(): {
-    error?: string;
-    uri?: string;
-  } {
-    const fileName = vscode.window.activeTextEditor?.document.fileName;
-    if (fileName) {
-      if (fileName.endsWith(".md")) {
-        const split = fileName.split(sep);
-        if (split.includes("content")) {
-          const rest = split.slice(split.findIndex((x) => x === "content") + 1);
-          // console.log({ rest });
-          const last = rest.pop();
-          if (last && last !== "index.md") {
-            rest.push(last.replace(/\.md$/, ""));
-          }
-          const uri = rest.join("/");
-          // console.log({ uri });
-
-          return { uri };
-        } else {
-          return {
-            error: "Current .md file is not inside a 'content' directory",
-          };
-        }
-      } else {
-        // vscode.window.showWarningMessage();
-        return { error: "Current file is not a .md file" };
-      }
-    } else {
-      return { error: "No current file name open" };
-    }
+  function getCurrentURI() {
+    return filenameToURI(vscode.window.activeTextEditor?.document.fileName);
   }
-
-  let openProd = vscode.commands.registerCommand("ghdocs-goer.openProd", () => {
-    const { uri, error } = getCurrentURI();
-    if (error) {
-      vscode.window.showWarningMessage(error);
-    } else {
-      const url = `https://docs.github.com/${uri}`;
-      vscode.env.openExternal(vscode.Uri.parse(url));
-    }
-  });
-  context.subscriptions.push(openProd);
-
-  let openLocalhost = vscode.commands.registerCommand(
-    "ghdocs-goer.openLocalhost",
-    () => {
+  function openExternal(prefix: string) {
+    return () => {
       const { uri, error } = getCurrentURI();
       if (error) {
         vscode.window.showWarningMessage(error);
       } else {
-        const url = `http://localhost:4000/${uri}`;
+        const url = `${prefix}${uri}`;
         vscode.env.openExternal(vscode.Uri.parse(url));
       }
-    }
+    };
+  }
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "ghdocs-goer.openProd",
+      openExternal("https://docs.github.com/")
+    )
   );
-  context.subscriptions.push(openLocalhost);
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "ghdocs-goer.openLocalhost",
+      openExternal("http://localhost:4000/")
+    )
+  );
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
+function filenameToURI(fileName: string | undefined): {
+  error?: string;
+  uri?: string;
+} {
+  if (fileName) {
+    if (fileName.endsWith(".md")) {
+      const split = fileName.split(sep);
+      if (split.includes("content")) {
+        const rest = split.slice(split.findIndex((x) => x === "content") + 1);
+        const last = rest.pop();
+        if (last && last !== "index.md") {
+          rest.push(last.replace(/\.md$/, ""));
+        }
+        const uri = rest.join("/");
+        return { uri };
+      } else {
+        return {
+          error: "Current .md file is not inside a 'content' directory",
+        };
+      }
+    } else {
+      return { error: "Current file is not a .md file" };
+    }
+  } else {
+    return { error: "No current file name open" };
+  }
+}
