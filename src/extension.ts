0dc416286764cf87612005c6a9df760610ea49c0
Peter Bengtsson <mail@peterbe.com>
2022-09-08 08:05:12 -0400

    semis

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
-import { sep } from "path";
+import { sep } from "path"
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from "vscode";
+import * as vscode from "vscode"
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -11,18 +11,19 @@ export function activate(context: vscode.ExtensionContext) {
   // console.log('Congratulations, your extension "ghdocs-goer" is now active!');
 
   function getCurrentURI() {
-    return filenameToURI(vscode.window.activeTextEditor?.document.fileName);
+    return filenameToURI(vscode.window.activeTextEditor?.document.fileName)
   }
+
   function openExternal(prefix: string) {
     return () => {
-      const { uri, error } = getCurrentURI();
+      const { uri, error } = getCurrentURI()
       if (error) {
-        vscode.window.showWarningMessage(error);
+        vscode.window.showWarningMessage(error)
       } else {
-        const url = `${prefix}${uri}`;
-        vscode.env.openExternal(vscode.Uri.parse(url));
+        const url = `${prefix}${uri}`
+        vscode.env.openExternal(vscode.Uri.parse(url))
       }
-    };
+    }
   }
 
   context.subscriptions.push(
@@ -30,43 +31,50 @@ export function activate(context: vscode.ExtensionContext) {
       "ghdocs-goer.openProd",
       openExternal("https://docs.github.com/")
     )
-  );
+  )
 
   context.subscriptions.push(
     vscode.commands.registerCommand(
       "ghdocs-goer.openLocalhost",
       openExternal("http://localhost:4000/")
     )
-  );
+  )
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "ghdocs-goer.openLocalhost",
+      openExternal("http://localhost:4000/")
+    )
+  )
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
 
 function filenameToURI(fileName: string | undefined): {
-  error?: string;
-  uri?: string;
+  error?: string
+  uri?: string
 } {
   if (fileName) {
     if (fileName.endsWith(".md")) {
-      const split = fileName.split(sep);
+      const split = fileName.split(sep)
       if (split.includes("content")) {
-        const rest = split.slice(split.findIndex((x) => x === "content") + 1);
-        const last = rest.pop();
+        const rest = split.slice(split.findIndex((x) => x === "content") + 1)
+        const last = rest.pop()
         if (last && last !== "index.md") {
-          rest.push(last.replace(/\.md$/, ""));
+          rest.push(last.replace(/\.md$/, ""))
         }
-        const uri = rest.join("/");
-        return { uri };
+        const uri = rest.join("/")
+        return { uri }
       } else {
         return {
           error: "Current .md file is not inside a 'content' directory",
-        };
+        }
       }
     } else {
-      return { error: "Current file is not a .md file" };
+      return { error: "Current file is not a .md file" }
     }
   } else {
-    return { error: "No current file name open" };
+    return { error: "No current file name open" }
   }
 }
